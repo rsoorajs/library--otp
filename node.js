e41dd24207314606f06c3e01792aadb0abd4f327
Sooraj <rsoorajs@gmail.com>
2022-10-31 19:07:37 +0530

    intial commit

---
+++ node.js
@@ -0,0 +1,13 @@
+// Random OTP Generator
+
+const otp = (limit) => {
+    const numbers = "0123456789";
+    let generatedOtp = "";
+
+    for (let i = 0; i < limit; i++) {
+        generatedOtp += numbers[Math.floor(Math.random() * 10)];
+    }
+    return generatedOtp;
+};
+
+module.exports = otp;
